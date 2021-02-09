144bf9706a53ecc3c3a372ef013a7c7e360fc5bf
amgodoi <amgodoi@gmail.com>
2021-02-08 22:34:22 -0300

    Initial commit.

---
+++ src/main.c
@@ -0,0 +1,18 @@
+/**
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <assert.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    printf("Hello, world!\n");
+
+	return 0;
+}
+
+
